a3d1a9c69d5d4b67d1ef60442a2168a008ab5c8c
Christopher Dent <christopherdent01@gmail.com>
2019-08-06 02:03:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -135,12 +135,16 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash each.do |season, hol|
+  holiday_hash.each do |season, hol|
+    
+    hol.each do |holiday, supplies|
+      
+    
     
      binding.pry 
     
    
-  
+ end 
 end
 
 
